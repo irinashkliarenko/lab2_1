7d7b1da8b6db880f13c31bdf415ed8e65952f7e2
Adam Rezner 195047 <adam.rezner95@gmail.com>
2017-03-14 10:54:42 -0700

    testy_v1 testCase1 Added

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class BinarySearchTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCase1() {
+		
+		int key = 3;
+		int[] seq = {3};
+		SearchResult result;
+		int position = 1;
+		
+		result = BinarySearch.search(key, seq);
+		assertTrue(result.isFound());
+		assertEquals(position, result.getPosition());
 	}
-
+	
+	
 }
