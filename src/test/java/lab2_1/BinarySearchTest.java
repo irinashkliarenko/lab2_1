b1eb9b8d469806e90287d191c4857ed77e083bc2
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-14 22:49:15 +0100

    [testy_v3] Dodanie testu uwzględniając algorytm wyszukiwania binarnego (2 przypadek) i poprawienie 1 przypadku

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -81,9 +81,20 @@ public class BinarySearchTest {
 
     @Test
     public void binarySearchCheckSubOnePlusSeqLenght(){
-        final int seq[] = new int[]{2, 4, 6, 8, 10, 12, 24};
+        final int seq[] = new int[]{2, 4, 6, 8, 10, 12, 15};
         final int key = 4;
-        final int position = 7 / 2 - 1;
+        final int position = (7 / 2) - 1;
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.isFound(), is(true));
+        System.out.println(searchResult.getPosition());
+        assertThat(searchResult.getPosition(), is(position));
+    }
+
+    @Test
+    public void binarySearchCheckPlusOnePlusSeqLenght(){
+        final int seq[] = new int[]{2, 4, 6, 8, 10, 12, 24, 30};
+        final int key = 10;
+        final int position = 8 / 2 + 1;
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertThat(searchResult.isFound(), is(true));
         assertThat(searchResult.getPosition(), is(position));
