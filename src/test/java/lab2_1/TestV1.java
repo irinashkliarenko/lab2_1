be5a777ce4531bf7c513ea2aa384a7f2695a9db4
195059 <195059@CARBON.CTI.P.LODZ.PL>
2017-03-14 18:02:11 +0100

    testy_v2 - 1. Dywersyfikacja danych

--- src/test/java/lab2_1/TestV1.java
+++ src/test/java/lab2_1/TestV1.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import edu.iis.mto.bsearch.BinarySearch;
 
 public class TestV1 {
-
+	
 	@Test
 	public void seq1Present() {
 		int [] array = new int[1];
@@ -19,7 +19,7 @@ public class TestV1 {
 	@Test
 	public void seq1NotPresent() {
 		int [] array = new int[1];
-		array[0] = 3;
+		array[0] = 5;
 		
 		assertFalse(BinarySearch.search(4, array).isFound());
 	}
@@ -28,18 +28,18 @@ public class TestV1 {
 	public void multipleSeqIsFirst() {
 		int [] array = new int[10];
 		for(int i = 0; i < 10; i++) {
-			array[i] = i;
+			array[i] = i+10;
 		}
-		assertTrue(BinarySearch.search(0, array).getPosition() == 1);
+		assertTrue(BinarySearch.search(10, array).getPosition() == 1);
 	}
 	
 	@Test
 	public void multipleSeqIsLast() {
-		int [] array = new int[10];
-		for(int i = 0; i < 10; i++) {
+		int [] array = new int[13];
+		for(int i = 0; i < 13; i++) {
 			array[i] = i;
 		}
-		assertTrue(BinarySearch.search(9, array).getPosition() == array.length);
+		assertTrue(BinarySearch.search(array.length-1, array).getPosition() == array.length);
 	}
 	
 	@Test
@@ -53,8 +53,8 @@ public class TestV1 {
 	
 	@Test
 	public void multipleSeqNotPresent() {
-		int [] array = new int[9];
-		for(int i = 0; i < 9; i++) {
+		int [] array = new int[6];
+		for(int i = 0; i < 6; i++) {
 			array[i] = i;
 		}
 		assertFalse(BinarySearch.search(9, array).isFound());
