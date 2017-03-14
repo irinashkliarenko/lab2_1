7a5735fcbcb58e7d4aed3944e394d2c543dfa1f4
Szyrner.Miłosz <195069@edu.p.lodz.pl>
2017-03-14 10:21:13 -0700

    testy_v1 Test: dlugosc sekwencji: 1, element: jest w sekwencji

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -2,13 +2,20 @@ package lab2_1;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSeqOneElementKeyExists() {
+		int key = 1;
+		int seq[] = {1};
+		SearchResult searchResult = BinarySearch.search(key, seq);
+		Assert.assertTrue(searchResult.isFound());
 	}
 
 }
