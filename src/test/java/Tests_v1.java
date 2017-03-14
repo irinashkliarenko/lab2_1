e1a3d190f7bcda45dc6d0faaf290d90b6d958240
wierzynski.patryk <195086@edu.p.lodz.pl>
2017-03-14 18:25:16 +0100

    testy_v1 - przypadek 2

--- src/test/java/Tests_v1.java
+++ src/test/java/Tests_v1.java
@@ -15,4 +15,13 @@ public class Tests_v1 {
         assertTrue(BinarySearch.search(3, array).getPosition() == 0);
     }
 
+    @Test
+    public void singleElementNotPresent() {
+        int[] array = new int[1];
+        array[0] = 3;
+
+        assertFalse(BinarySearch.search(15, array).isFound());
+        assertTrue(BinarySearch.search(15, array).getPosition() == -1);
+    }
+
 }
