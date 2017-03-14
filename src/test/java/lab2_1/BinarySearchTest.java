d033cc4525e4ecb0ca7742f2bb3c4fb2f12234a3
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-03-14 10:46:01 -0700

    Usuniecie metody do sortowania, bo jednak nie potrzebna

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -17,15 +17,6 @@ public class BinarySearchTest {
 		binarySearch = new BinarySearch();		
 	}
 	
-	public boolean isSeqSorted(int[] seq){
-		for (int i = 0; i < seq.length - 1; i++) {
-	        if (seq[i] > seq[i + 1]) {
-	            return false; 
-	        }
-	    }
-	    return true;
-	}
-	
 	@Test
 	public void sequenceSortTest(){
 		
