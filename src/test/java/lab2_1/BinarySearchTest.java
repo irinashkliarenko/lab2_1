f2de758af4bb9c2ccda72729b0f914df191f3429
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-03-14 10:36:32 -0700

    Dodanie metody do sprawdzenia czy seq jest posortowany

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package lab2_1;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
 
@@ -8,11 +9,25 @@ public class BinarySearchTest {
 	
 	private BinarySearch binarySearch;
 	private int key;
-	private int[] seq;
+	private int[] seq1;
+	private int[] seq2;
 
 	@Before
 	public void setup(){
-		binarySearch = new BinarySearch();
+		binarySearch = new BinarySearch();		
+	}
+	
+	public boolean isSeqSorted(int[] seq){
+		for (int i = 0; i < seq.length - 1; i++) {
+	        if (seq[i] > seq[i + 1]) {
+	            return false; 
+	        }
+	    }
+	    return true;
+	}
+	
+	@Test
+	public void sequenceSortTest(){
 		
 	}
 }
