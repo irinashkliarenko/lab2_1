1b52097db19d5d3f7cf286b30a73c87718021d90
patrykxk <patrix215@gmail.com>
2017-03-14 10:59:31 -0700

    testy_v2 poprawa bledow w emptySequence i Testy_v1

--- src/test/java/lab2_1/Testy_v2.java
+++ src/test/java/lab2_1/Testy_v2.java
@@ -8,12 +8,15 @@ import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 
 public class Testy_v2 {
-	@Test
+	private int[] seq = {1,2,3,4,5,6,7,8};
+	private int key = 3;
+	
+	@Test(expected = IllegalArgumentException.class)
 	public void notInLongerSequence() {
-		int[] seq = {1,2,3,4,5};
-		int key = 3;
-		
+		int[] seq = {};
 		SearchResult result = BinarySearch.search(key, seq);
 		assertTrue(result.isFound());
 	}
+	
+	
 }
