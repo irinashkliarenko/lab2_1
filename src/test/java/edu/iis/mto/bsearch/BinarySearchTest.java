e108a264c986242459b6968e7f2d30665d474b1f
Maciek <miedzianowski.maciek@gmail.com>
2017-03-14 18:02:33 +0100

    testy_v1 / Dodanie testów dla drugiego przypadku

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -11,6 +11,7 @@ import static org.hamcrest.Matchers.*;
 public class BinarySearchTest {
 
     final int ANY_KEY = 12;
+    final int ANY_KEY_2 = 14;
 
     @Test
     public void shouldFindElement() throws Exception {
@@ -35,5 +36,30 @@ public class BinarySearchTest {
 
         //then:
         assertThat(sut.getPosition(),is(EXPECTED_KEY_POSITION));
+    }
+
+    @Test
+    public void shouldNotFindElement() throws Exception {
+        //given:
+        final int[] ARRAY_WITH_KEY = new int[]{ANY_KEY_2};
+
+        //when:
+        SearchResult sut = BinarySearch.search(ANY_KEY,ARRAY_WITH_KEY);
+
+        //then:
+        assertThat(sut.isFound(),is(false));
+    }
+
+    @Test
+    public void shouldNotSetPositionOfKey() throws Exception {
+        //given:
+        final int[] ARRAY_WITH_KEY = new int[]{ANY_KEY_2};
+        final int INDEX_NOT_FOUND = -1;
+
+        //when:
+        SearchResult sut = BinarySearch.search(ANY_KEY,ARRAY_WITH_KEY);
+
+        //then:
+        assertThat(sut.getPosition(),is(INDEX_NOT_FOUND));
     }
 }
