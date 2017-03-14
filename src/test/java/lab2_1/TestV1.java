734ef37729ccf97778d0dc27cdee4632cdfcac2a
patrykxk <patrix215@gmail.com>
2017-03-14 10:13:20 -0700

    testy_v1 Dodano drugi test - brak w sekwencji o dlugosci 1

--- src/test/java/lab2_1/TestV1.java
+++ src/test/java/lab2_1/TestV1.java
@@ -10,12 +10,21 @@ import edu.iis.mto.bsearch.SearchResult;
 public class TestV1 {
 
 	@Test
-	public void test1() {
+	public void inSequenceLength1() {
 		int[] seq = {1};
 		int key = 1;
 		
 		SearchResult result = BinarySearch.search(key, seq);
 		assertTrue(result.isFound());
+	}
+	
+	@Test
+	public void notinSequenceLength1() {
+		int[] seq = {2};
+		int key = 1;
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		assertFalse(result.isFound());
 	}
 
 }
