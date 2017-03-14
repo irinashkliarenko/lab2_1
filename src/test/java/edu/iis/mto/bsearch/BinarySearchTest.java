1a3bd501c0b05a88c4827eb94499bc5ceab47daa
Gos.Konrad <194948@edu.p.lodz.pl>
2017-03-14 18:57:29 +0100

    implementing method for testing if seq is founded - first assertEquals

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.bsearch;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -12,10 +13,18 @@ public class BinarySearchTest {
     private BinarySearch binarySearch;
     private int key;
     private int[] seq;
-    
+
     @Before
     public void setUp() throws Exception {
         binarySearch = new BinarySearch();
     }
 
+    @Test
+    public void isInputSeqFound() {
+        key = 1;
+        seq = new int[] {1, 2, 4, 6};
+
+        assertEquals(true,BinarySearch.search(key, seq).isFound());
+        System.out.print(seq[1]);
+    }
 }
