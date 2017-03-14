a85dec5a1d67bdd20cab8a988724bd4737167bc4
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-03-14 09:51:24 -0700

    Utworzenie klasy do testow i stworzenie setup

---
+++ src/test/java/lab2_1/SearchResultTest.java
@@ -0,0 +1,15 @@
+package lab2_1;
+
+import org.junit.Before;
+
+import edu.iis.mto.bsearch.SearchResult;
+
+public class SearchResultTest {
+	
+	private SearchResult searchResult;
+
+	@Before
+	public void setup(){
+		searchResult = new SearchResult();
+	}
+}
