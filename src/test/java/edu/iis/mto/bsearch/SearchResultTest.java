0469dee81f3ff408ba3dc18cbbde1b8575327901
Gos.Konrad <194948@edu.p.lodz.pl>
2017-03-14 17:52:49 +0100

    added gitignore and class for testing search with setUp

---
+++ src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Konrad Gos on 14.03.2017.
+ */
+public class SearchResultTest {
+
+    private SearchResult searchResult;
+
+    @Before
+    public void setUp() throws Exception {
+        searchResult = new SearchResult();
+    }
+
+}
