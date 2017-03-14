cf8bf197a14f1c20112df0208c7614cd527e4b89
Patryk <patroneq@gmail.com>
2017-03-14 10:47:35 -0700

    testy_v1 Utworzenie testu dla 2 przypadku i dodanie komentarzy

--- src/test/java/edu/iis/mto/bsearch/tests/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/tests/BinarySearchTest.java
@@ -11,11 +11,25 @@ public class BinarySearchTest {
 
 	@Test
 	public void testBinarySearchCase1() {
+		//dlugosc sekwencji wejsciowej: 1
+		//element wyszukiwany: jest w sekwencji
 		int key = 5;
 		int[] seq = {5};
 		int position = 1;
 		SearchResult result = BinarySearch.search(key, seq);
 		assertTrue(result.isFound());
+		assertEquals(position, result.getPosition());
+	}
+	
+	@Test
+	public void testBinarySearchCase2() {
+		//dlugosc sekwencji wejsciowej: 1
+		//element wyszukiwany: nie ma w sekwencji
+		int key = 5;
+		int[] seq = {4};
+		int position = -1;
+		SearchResult result = BinarySearch.search(key, seq);
+		assertTrue(!result.isFound());
 		assertEquals(position, result.getPosition());
 	}
 
