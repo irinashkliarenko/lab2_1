b815e8162aba06f120d18a79890d9a535563f8c3
Szyrner.Miłosz <195069@edu.p.lodz.pl>
2017-03-14 09:54:43 -0700

    testy_v1 Utworzenie klasy BinarySearchTest

---
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package lab2_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
