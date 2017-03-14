a2ca67fd177ed027f9c02f83a87a838b299d1dd2
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-14 10:30:10 -0700

    testy_v1) dodano test keyIsLastInTheSequence, poprawa bledow w innych testach

--- src/test/java/lab2_1/testy_v1.java
+++ src/test/java/lab2_1/testy_v1.java
@@ -43,11 +43,22 @@ public class testy_v1 {
 		BinarySearch binarySearch = new BinarySearch();
 		SearchResult result = binarySearch.search(key, seq);
 		
-		assertTrue(result.isFound());
 		assertEquals(1, result.getPosition());
 		
 	}
 	
+	@Test
+	public void keyIsLastInTheSequence() {
+		
+		int key = 7;
+		int[] seq = { 2, 3, 4, 5, 6, 7 } ;
+		BinarySearch binarySearch = new BinarySearch();
+		SearchResult result = binarySearch.search(key, seq);
+		
+		assertEquals(seq.length, result.getPosition());
+		
+		
+	}
 	
 	
 	
