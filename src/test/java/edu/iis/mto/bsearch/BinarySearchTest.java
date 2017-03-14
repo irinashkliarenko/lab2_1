46c505d4dacfe953b3ca39e93b46993a57e85413
AnnaKam <194974@edu.p.lodz.pl>
2017-03-14 17:52:42 +0100

    testy_v1 case2 test added

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -17,4 +17,13 @@ public class BinarySearchTest {
         assertTrue(searchResult.isFound());
         assertEquals(0, searchResult.getPosition());
     }
+
+    @Test
+    public void oneElement_NotIn() throws Exception{
+        int element = 11;
+        int [] tab = new int [] {10};
+        SearchResult searchResult = BinarySearch.search(element,tab);
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
+    }
 }
