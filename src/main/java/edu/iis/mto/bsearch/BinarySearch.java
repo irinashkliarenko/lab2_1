e63e56e0e1cc7382124edb88abb8558b775e078f
Robert Pakowski <ropak@wp.pl>
2017-03-14 18:25:59 +0100

    testy_v2 * repaired search method: added throwing exception

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -21,6 +21,11 @@ public class BinarySearch {
 	 *         znaleziony -1)
 	 */
 	public static SearchResult search(int key, int[] seq) {
+
+		if(seq.length < 1) {
+			throw new IllegalArgumentException("Passed sequence cannot be empty");
+		}
+
 		int start = 0;
 		int end = seq.length - 1;
 		int center;
