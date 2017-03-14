cb37f2f20c9bb904ed37925d7854743ad79afb21
Adam Rezner 195047 <adam.rezner95@gmail.com>
2017-03-14 10:06:10 -0700

    testy_v1 BinarySearchTest class added

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.bsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
