dda4f5fb59ff48e0762cde391eb5e98ca06dde8c
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-03-14 10:13:24 -0700

    Dodanie zmiennych wzorujac sie na sygnaturze

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -7,9 +7,12 @@ import edu.iis.mto.bsearch.BinarySearch;
 public class BinarySearchTest {
 	
 	private BinarySearch binarySearch;
+	private int key;
+	private int[] seq;
 
 	@Before
 	public void setup(){
 		binarySearch = new BinarySearch();
+		
 	}
 }
