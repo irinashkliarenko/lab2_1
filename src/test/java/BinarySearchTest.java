188c3886e55f8692a5a1bbe03f509c407c81c9c6
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-14 18:22:44 +0100

    testy_v1 Create test: value is first in more than one-element sequence.

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -25,4 +25,15 @@ public class BinarySearchTest {
         SearchResult result = BinarySearch.search(value, seq);
         Assert.assertFalse(result.isFound());
     }
+
+    @Test
+    public void testValueIsFirstElementInSeq() {
+        int[] seq = {1, 2, 3};
+        int value = 1;
+        final int EXPECTED_VALUE = 0;
+
+        SearchResult result = BinarySearch.search(value, seq);
+        Assert.assertTrue(result.isFound());
+        Assert.assertEquals(EXPECTED_VALUE, result.getPosition());
+    }
 }
