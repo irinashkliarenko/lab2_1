c66608cf2ca9679b33cdb35faa2be67636295588
Aleksander Kaczmarczyk <194972@edu.p.lodz.pl>
2017-03-14 09:56:04 -0700

    testy_v1 element jest w sekwencji dla dlugosci 1

--- src/test/java/lab2_1/TestyV1.java
+++ src/test/java/lab2_1/TestyV1.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+
 public class TestyV1 {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testKeyIsInSequence() {
+		
+		int[] testSeq = {1};
+		int key = 1;
+		
+		assertTrue(testSeq.length == 1);
+		
+		SearchResult result = BinarySearch.search(key, testSeq);
+		assertTrue(result.isFound());
 	}
 
 }
