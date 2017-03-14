cf4360fbcc8ada9ab7d73aa000aa62f8faa756a5
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-14 20:46:19 +0100

    [testy_v1] Dodanie testu dla seq o dlugosci >1 i elementu znajdujacego sie w sekwencji na pierwszym miejscu oraz rozszerzenie metody setup

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package lab2_1;
 
+import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +10,14 @@ import edu.iis.mto.bsearch.BinarySearch;
 public class BinarySearchTest {
 	
 	private BinarySearch binarySearch;
+	private SearchResult searchResult;
 	private int key;
 	private int[] seq;
 
 	@Before
 	public void setup(){
-		binarySearch = new BinarySearch();		
+		binarySearch = new BinarySearch();
+		searchResult = new SearchResult();
 	}
 	
     @Test
@@ -30,4 +33,14 @@ public class BinarySearchTest {
 		key = 1;
 		Assert.assertFalse(binarySearch.search(key, seq).isFound());
 	}
+
+	@Test
+    public void checkIfKeyIsFirstElementOfSeq(){
+	    seq = new int[]{2, 4, 6, 8, 10, 12, 24};
+	    key = 2;
+	    int position = 1;
+	    searchResult = binarySearch.search(key, seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals(position, searchResult.getPosition());
+    }
 }
