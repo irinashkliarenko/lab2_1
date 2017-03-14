77229ab86800b3970bc8b6e2df62de77f3e230e2
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-14 18:38:43 +0100

    testy_v1 add test elementIsFirstInSequency

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class BinarySearchTest {
 
-    int element = 10;
+    int element = 1000;
     int seq[];
     SearchResult searchResult;
 
@@ -30,6 +30,18 @@ public class BinarySearchTest {
         searchResult = BinarySearch.search(element, seq);
         assertThat(searchResult.isFound(), is(false));
     }
+
+    @Test
+    public void elementIsFirstInSequency() throws Exception {
+
+        seq = new int[] {element, element + 4556, element + 3};
+        searchResult = BinarySearch.search(element, seq);
+        assertThat(searchResult.isFound(), is(true));
+        assertThat(searchResult.getPosition(), is(0) );
+    }
+
+
+
 
 
 
