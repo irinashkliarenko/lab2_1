deae4e12587d4030f235dda39bd373189ceafb9d
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-15 12:43:47 +0100

    testy_v1 Added test case for seq.lenght()>1 and key is in the middle of sequence

--- src/test/java/ExpectedSearchResultTest.java
+++ src/test/java/ExpectedSearchResultTest.java
@@ -40,5 +40,13 @@ public class ExpectedSearchResultTest {
         Assert.assertTrue(result.getPosition()==seq.length);
     }
 
+    @Test public void testKeyIsInTheMiddleOfSequence (){
+        int key = 3;
+        int[] seq = {1,3,5};
+        SearchResult result = BinarySearch.search(key,seq);
+        Assert.assertTrue(result.isFound());
+        Assert.assertTrue(result.getPosition()==2);
+    }
+
 
 }
