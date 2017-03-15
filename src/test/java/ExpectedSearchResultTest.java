25ea542962bbec1ec962d2d8122b326ba86d5b53
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-15 12:38:00 +0100

    testy_v1 Added test case for seq.lenght()==1 and key is in sequence

--- src/test/java/ExpectedSearchResultTest.java
+++ src/test/java/ExpectedSearchResultTest.java
@@ -1,6 +1,18 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by grusz on 15.03.2017.
  */
 public class ExpectedSearchResultTest {
 
+    @Test public void testKeyIsInSequence (){
+        int key = 5;
+        int[] seq = {5};
+        SearchResult result = BinarySearch.search(key,seq);
+        Assert.assertTrue(result.isFound());
+        Assert.assertTrue(result.getPosition()==1);
+    }
 }
