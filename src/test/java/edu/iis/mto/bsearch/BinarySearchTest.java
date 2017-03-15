d6fc895bd3f2b4513cc320be23fc435ad241ee49
Adam Rezner <195047@edu.p.lodz.pl>
2017-03-15 17:55:13 +0100

    testy_v1 Comments for all tests added

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -10,6 +10,8 @@ public class BinarySearchTest {
 
 	@Test
 	public void testCase1() {
+		/*Sprawdzenie czy element wyszukiwany jest w sekwencji
+		 sekwencji wejœciowej =1*/
 		
 		int key = 3;
 		int[] seq = {3};
@@ -23,7 +25,9 @@ public class BinarySearchTest {
 	
 	@Test
 	public void testCase2() {
-		
+		/*sprawdzenie czy elementu wyszukiwanego 
+		nie ma w sekwencji przy sekwencji wejœciowej =1
+		*/
 		int key = 4;
 		int[] seq = {3};
 		SearchResult result;
@@ -36,11 +40,14 @@ public class BinarySearchTest {
 	
 	@Test
 	public void testCase3() {
+		/*sprawdzenie czy element wyszukiwany 
+		jest pierwszym elementem przy sekwencji wejœciowej >1
+		*/
 		
 		int key = 3;
 		int[] seq = {3, 10, 5, 8};
 		SearchResult result;
-		int position = 1;
+		int position = 1;	
 		
 		result = BinarySearch.search(key, seq);
 		assertTrue(result.isFound());
@@ -49,6 +56,9 @@ public class BinarySearchTest {
 	
 	@Test
 	public void testCase4() {
+		/*sprawdzenie czy element wyszukiwany 
+		jest ostatnim elementem przy sekwencji wejœciowej >1
+		*/
 		
 		int key = 3;
 		int[] seq = {8, 10, 5, 3};
