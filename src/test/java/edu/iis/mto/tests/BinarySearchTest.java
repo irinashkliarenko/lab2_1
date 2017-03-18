84cde6185a83fe641a321524d4cb9389adcf171d
Klaudia Głuszek <klaudia.gluszek@gmail.com>
2017-03-18 11:35:24 +0100

    testy_v1 element szukany jest na pierwszej pozycji

--- src/test/java/edu/iis/mto/tests/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/tests/BinarySearchTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.is;
  * User: Klaudia
  */
 public class BinarySearchTest {
-
+    int seq[] = new int[]{1, 2, 3, 4, 5, 6};
 
     @Test(expected = IllegalArgumentException.class)
     public void notContainsAnyElement() {
@@ -22,7 +22,6 @@ public class BinarySearchTest {
     @Test
     public void containsElementInSeq() {
         int key = 2;
-        int seq[] = new int[] {1,2,3,4,5,6};
         SearchResult searchResult = BinarySearch.search(key, seq);
         Assert.assertThat(searchResult.isFound(), is(true));
     }
@@ -31,34 +30,31 @@ public class BinarySearchTest {
     public void containsElementInSeqPosition() {
         int key = 2;
         int position = 1;
-        int seq[] = new int[] {1,2,3,4,5,6};
         SearchResult searchResult = BinarySearch.search(key, seq);
         Assert.assertEquals(key, seq[position]);
         Assert.assertThat(searchResult.getPosition(), is(position));
     }
 
     @Test
-    public void keyNotIncludedInSequence(){
+    public void keyNotIncludedInSequence() {
         int key = 7;
-        int seq[] = new int[] {1,2,3,4,5,6};
         SearchResult searchResult = BinarySearch.search(key, seq);
         Assert.assertThat(searchResult.isFound(), is(false));
     }
 
     @Test
-    public void keyNotIncludedInSequencePosition(){
+    public void keyNotIncludedInSequencePosition() {
         int key = 7;
         int position = -1;
-        int seq[] = new int[] {1,2,3,4,5,6};
         SearchResult searchResult = BinarySearch.search(key, seq);
         Assert.assertEquals(position, searchResult.getPosition());
     }
 
     @Test
-    public void isContentInSeqLenOne(){
+    public void isContentInSeqLenOne() {
         int key = 7;
         int seq[] = new int[]{key};
-        int pos = seq.length-1;
+        int pos = seq.length - 1;
         SearchResult searchResult = BinarySearch.search(key, seq);
         Assert.assertEquals(true, searchResult.isFound());
         Assert.assertEquals(pos, searchResult.getPosition());
@@ -66,12 +62,21 @@ public class BinarySearchTest {
 
 
     @Test
-    public void notContentInSeqLenOne(){
+    public void notContentInSeqLenOne() {
         int key = 7;
         int seq[] = new int[]{key};
-        int pos =  -1;
-        SearchResult searchResult = BinarySearch.search(key+1, seq);
+        int pos = -1;
+        SearchResult searchResult = BinarySearch.search(key + 1, seq);
         Assert.assertEquals(false, searchResult.isFound());
         Assert.assertEquals(pos, searchResult.getPosition());
     }
+
+    @Test
+    public void elementIsFirstInSeq() {
+        int key = 1;
+        int pos = 0;
+        SearchResult result = BinarySearch.search(key, seq);
+        Assert.assertEquals(pos, result.getPosition());
+    }
+
 }
