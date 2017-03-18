d8ad400ead010a57625b15bb76a794435e77215c
miloszszyrner <milosz.szyrner@gmail.com>
2017-03-18 21:02:13 +0100

    testy_v2 Refaktoryzacja testów, uzycie assertThat

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package lab2_1;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,52 +15,66 @@ public class BinarySearchTest {
 	public void testSeqOneElementKeyExists() {
 		int key = 1;
 		int seq[] = {1};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertTrue(searchResult.isFound());
+		
+		assertThat(searchResult.isFound(),is(true));
 	}
 	@Test
 	public void testSeqOneElementKeyNotFound() {
 		int key = 4;
 		int seq[] = {1};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertFalse(searchResult.isFound());
+		
+		assertThat(searchResult.isFound(),is(false));
 	}
 	@Test
 	public void testSeqMoreElementsKeyFirst() {
 		int key = 4;
 		int seq[] = {4,1};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertTrue(searchResult.isFound());
-		Assert.assertEquals(seq[searchResult.getPosition()], key);
+		
+		assertThat(searchResult.isFound(),is(true));
+		assertThat(seq[searchResult.getPosition()], is(key));
 	}
 	@Test
 	public void testSeqMoreElementsKeyLast() {
 		int key = 4;
 		int seq[] = {2,1,4};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertTrue(searchResult.isFound());
-		Assert.assertEquals(seq[searchResult.getPosition()], key);
+		
+		assertThat(searchResult.isFound(),is(true));
+		assertThat(seq[searchResult.getPosition()], is(key));
 	}
 	@Test
 	public void testSeqMoreElementsKeyCenter() {
 		int key = 4;
 		int seq[] = {2,1,4,3,6};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertTrue(searchResult.isFound());
-		Assert.assertEquals(seq[searchResult.getPosition()], key);
+
+		assertThat(searchResult.isFound(),is(true));
+		assertThat(seq[searchResult.getPosition()], is(key));
 	}
 	@Test
 	public void testSeqMoreElementsKeyNotFound() {
 		int key = 5;
 		int seq[] = {2,1,4,3,6};
+		int INDEX_NOT_FOUND = -1;
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
-		Assert.assertFalse(searchResult.isFound());
-		Assert.assertEquals(searchResult.getPosition(), -1);
+		
+		assertThat(searchResult.isFound(), is(false));
+		assertThat(searchResult.getPosition(), is(INDEX_NOT_FOUND));
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void testSeqZeroElements() {
 		int key = 5;
 		int seq[] = {};
+		
 		SearchResult searchResult = BinarySearch.search(key, seq);
 	}
 }
