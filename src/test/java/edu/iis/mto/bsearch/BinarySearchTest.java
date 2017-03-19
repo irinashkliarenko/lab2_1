60a4e1cbb660334de5e94aec2919f39a45f84fe9
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-19 16:16:17 +0100

    testy_v3 dodanie testu sprawdzajego poprawnosc dzialania dla sekwencji o parzystej liczbie elementow

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -13,7 +13,8 @@ public class BinarySearchTest {
 	
 	private final int[] seqWithZeroElements = {};
 	private final int[] seqWithOneElement = {1};
-	private final int[] seqWithManyElements = {1,3,5};
+	private final int[] seqWithThreeElements = {1,3,5};
+	private final int[] seqWithFourElements = {1,3,5,7};
 	
 	@Test
 	public void testKeyIsFoundInSeqWithOneElement() {
@@ -41,54 +42,54 @@ public class BinarySearchTest {
 	}
 	
 	@Test
-	public void testKeyIsFirstElementInSeqWithManyElements() {
+	public void testKeyIsFirstElementInSeqWithThreeElements() {
 		
 		int key = 1;
 		
-		Assert.assertThat(seqWithManyElements.length, greaterThan(1));
+		Assert.assertThat(seqWithThreeElements.length, greaterThan(1));
 		
-		SearchResult result = BinarySearch.search(key, seqWithManyElements);
+		SearchResult result = BinarySearch.search(key, seqWithThreeElements);
 		Assert.assertThat(1, is(result.getPosition()));
-		Assert.assertThat(seqWithManyElements[result.getPosition()-1], is(key));
+		Assert.assertThat(seqWithThreeElements[result.getPosition()-1], is(key));
 	}
 	
 	@Test
-	public void testKeyIsLastElementInSeqWithManyElements() {
+	public void testKeyIsLastElementInSeqWithThreeElements() {
 		
 		int key = 5;
 		
-		Assert.assertThat(seqWithManyElements.length, greaterThan(1));
+		Assert.assertThat(seqWithThreeElements.length, greaterThan(1));
 		
-		SearchResult result = BinarySearch.search(key, seqWithManyElements);
-		Assert.assertThat(seqWithManyElements.length, is(result.getPosition()));
-		Assert.assertThat(seqWithManyElements[result.getPosition()-1], is(key));
+		SearchResult result = BinarySearch.search(key, seqWithThreeElements);
+		Assert.assertThat(seqWithThreeElements.length, is(result.getPosition()));
+		Assert.assertThat(seqWithThreeElements[result.getPosition()-1], is(key));
 	}
 	
 	@Test
-	public void testKeyIsCenterElementInSeqWithManyElements() {
+	public void testKeyIsCenterElementInSeqWithThreeElements() {
 		
 		int key = 3;
 		
-		Assert.assertThat(seqWithManyElements.length, greaterThan(1));
+		Assert.assertThat(seqWithThreeElements.length, greaterThan(1));
 		
-		SearchResult result = BinarySearch.search(key, seqWithManyElements);
+		SearchResult result = BinarySearch.search(key, seqWithThreeElements);
 		int start = 0;
-		int end = seqWithManyElements.length - 1;
+		int end = seqWithThreeElements.length - 1;
 		int center = (start + end) / 2;
 		
 		Assert.assertThat(center+1, is(result.getPosition()));
-		Assert.assertThat(seqWithManyElements[result.getPosition()-1], is(key));
+		Assert.assertThat(seqWithThreeElements[result.getPosition()-1], is(key));
 	}
 	
 	@Test
-	public void testKeyIsNotFoundInSeqWithManyElements() {
+	public void testKeyIsNotFoundInSeqWithThreeElements() {
 
 		int key = 2;
 		final int KEY_NOT_FOUND_INDICATOR = -1;
 		
-		Assert.assertThat(seqWithManyElements.length, greaterThan(1));
+		Assert.assertThat(seqWithThreeElements.length, greaterThan(1));
 		
-		SearchResult result = BinarySearch.search(key, seqWithManyElements);
+		SearchResult result = BinarySearch.search(key, seqWithThreeElements);
 		Assert.assertThat(result.isFound(), is(false));
 		Assert.assertThat(result.getPosition(), is(KEY_NOT_FOUND_INDICATOR));
 	}
@@ -98,5 +99,17 @@ public class BinarySearchTest {
 		
 		int key = 1;
 		BinarySearch.search(key, seqWithZeroElements);
+	}
+	
+	@Test
+	public void testKeyIsFoundInSeqWithFourElements() {
+		
+		int key = 5;
+		
+		Assert.assertThat(seqWithThreeElements.length, greaterThan(1));
+		
+		SearchResult result = BinarySearch.search(key, seqWithFourElements);
+		Assert.assertThat(3, is(result.getPosition()));
+		Assert.assertThat(seqWithThreeElements[result.getPosition()-1], is(key));
 	}
 }
