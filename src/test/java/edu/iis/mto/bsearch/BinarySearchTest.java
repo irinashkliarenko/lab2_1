9a23fbecc235e602b23364989cf1fcfcc86b81ca
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-20 21:34:03 +0100

    testy_v3 refactored tests  elementIsInMiddlePlusOneOfSequence elementIsInMiddleMinusOneOfSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -85,16 +85,16 @@ public class BinarySearchTest {
     @Test
     public void elementIsInMiddlePlusOneOfSequence() throws Exception {
 
-        seq = new int[] {element - 235, element - 53, element - 2, element, element + 56, element + 666};
+        seq = new int[] {element - 235, element - 53, element - 1, element, element + 666};
         searchResult = BinarySearch.search(element, seq);
         assertThat(searchResult.isFound(), is(true));
-        assertThat(searchResult.getPosition(), equalTo(2+1));
+        assertThat(searchResult.getPosition(), equalTo(3));
     }
 
     @Test
     public void elementIsInMiddleMinusOneOfSequence() throws Exception {
 
-        seq = new int[] {element - 235, element, element + 56, element + 666};
+        seq = new int[] {element - 235, element, element + 1, element + 56, element + 666};
         searchResult = BinarySearch.search(element, seq);
         assertThat(searchResult.isFound(), is(true));
         assertThat(searchResult.getPosition(), equalTo(2-1));
