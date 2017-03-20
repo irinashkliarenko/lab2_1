a8fa1b9da7de8c524713bba959497f7cd11292cd
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-20 19:46:01 +0100

    testy_v2) Zmiana nazw sekwencji testowych

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -15,8 +15,8 @@ import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest {
 
-	int[] seq = {  3, 4, 5, 6,  7, 8, 9, 10, 11, 12};
-	int[] seq2 = {  3, 4, 5, 6,  7, 8, 9, 10, 11, 12, 13};
+	int[] evenSeq = {  3, 4, 5, 6,  7, 8, 9, 10, 11, 12 };
+	int[] oddSeq = {  3, 4, 5, 6,  7, 8, 9, 10, 11, 12, 13 };
 
 	
 	@Test
@@ -50,7 +50,7 @@ public class BinarySearchTest {
 		
 		int key = 3;
 		BinarySearch binarySearch = new BinarySearch();
-		SearchResult result = binarySearch.search(key, seq);
+		SearchResult result = binarySearch.search(key, evenSeq);
 		
 		Assert.assertThat(result.isFound(), is(true));
 		Assert.assertThat(result.getPosition(), is(1));
@@ -62,10 +62,10 @@ public class BinarySearchTest {
 		
 		int key = 12;
 		BinarySearch binarySearch = new BinarySearch();
-		SearchResult result = binarySearch.search(key, seq);
+		SearchResult result = binarySearch.search(key, evenSeq);
 		
 		Assert.assertThat(result.isFound(), is(true));
-		Assert.assertThat(result.getPosition(), is(seq.length));
+		Assert.assertThat(result.getPosition(), is(evenSeq.length));
 		
 		
 	}
@@ -75,10 +75,10 @@ public class BinarySearchTest {
 		
 		int key = 7;
 		BinarySearch binarySearch = new BinarySearch();
-		SearchResult result = binarySearch.search(key, seq);
+		SearchResult result = binarySearch.search(key, evenSeq);
 		
 		Assert.assertThat(result.isFound(), is(true));
-		Assert.assertThat(result.getPosition(), is(seq.length/2));
+		Assert.assertThat(result.getPosition(), is(evenSeq.length/2));
 	}
 	
 	@Test
@@ -86,10 +86,10 @@ public class BinarySearchTest {
 		
 		int key = 7;
 		BinarySearch binarySearch = new BinarySearch();
-		SearchResult result = binarySearch.search(key, seq2);
+		SearchResult result = binarySearch.search(key, oddSeq);
 		
 		Assert.assertThat(result.isFound(), is(true));
-		Assert.assertThat(result.getPosition(), is(seq2.length/2));
+		Assert.assertThat(result.getPosition(), is(oddSeq.length/2));
 	}
 	
 	@Test
@@ -97,7 +97,7 @@ public class BinarySearchTest {
 		
 		int key = 15;
 		BinarySearch binarySearch = new BinarySearch();
-		SearchResult result = binarySearch.search(key, seq);
+		SearchResult result = binarySearch.search(key, evenSeq);
 		
 		Assert.assertThat(result.isFound(), is(not(true)));
 		Assert.assertThat(result.getPosition(), is(-1));
