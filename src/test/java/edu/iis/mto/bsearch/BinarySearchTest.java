301ce19ecc81e093050f116215eb944bc9034e82
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-20 13:42:21 +0100

    testy_v3 add test elementIsInMiddlePlusOneOfSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -81,4 +81,13 @@ public class BinarySearchTest {
         assertThat(searchResult.isFound(), is(true));
         assertThat(searchResult.getPosition(), equalTo(2));
     }
+
+    @Test
+    public void elementIsInMiddlePlusOneOfSequence() throws Exception {
+
+        seq = new int[] {element - 235, element - 53, element - 2, element, element + 56, element + 666};
+        searchResult = BinarySearch.search(element, seq);
+        assertThat(searchResult.isFound(), is(true));
+        assertThat(searchResult.getPosition(), equalTo(2+1));
+    }
 }
