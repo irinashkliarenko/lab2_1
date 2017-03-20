82c2e8ccc4248a5fb2e2be773ea7d9421bc3f8e8
Aleksandra Dura <aleksandradura_95@onet.pl>
2017-03-20 13:41:33 +0100

    [testy_v1]  Add test elementIsNotInSequence and changing test oneElementInSequence and oneElementIsNotInSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,21 +8,16 @@ import static org.junit.Assert.*;
  * Created by test on 14.03.2017.
  */
 public class BinarySearchTest {
-//    @Test(expected = IllegalArgumentException.class)
-//    public void searchLengthSequenceEqualZero() throws Exception {
-//        int[] arrayOfNumber = new int[]{};
-//        BinarySearch.search(0, arrayOfNumber);
-//    }
     @Test
-    public void elementIsInSequence() throws Exception {
-        int[] arrayOfNumber = new int[]{1,2,3,4};
-        SearchResult searchResult = BinarySearch.search(2, arrayOfNumber);
+    public void oneElementIsInSequence() throws Exception {
+        int[] arrayOfNumber = new int[]{1};
+        SearchResult searchResult = BinarySearch.search(1, arrayOfNumber);
         assertEquals(true, searchResult.isFound());
-        assertEquals(2, searchResult.getPosition());
+        assertEquals(1, searchResult.getPosition());
     }
     @Test
-    public void elementIsNotInSequence() throws Exception {
-        int[] arrayOfNumber = new int[]{1,2,3,4};
+    public void oneElementIsNotInSequence() throws Exception {
+        int[] arrayOfNumber = new int[]{1};
         SearchResult searchResult = BinarySearch.search(6, arrayOfNumber);
         assertEquals(false, searchResult.isFound());
         assertEquals(-1, searchResult.getPosition());
@@ -47,6 +42,13 @@ public class BinarySearchTest {
         SearchResult searchResult = BinarySearch.search(3, arrayOfNumber);
         assertEquals(true, searchResult.isFound());
         assertEquals(3, searchResult.getPosition());
+    }
+    @Test
+    public void elementIsNotInSequence() throws Exception {
+        int[] arrayOfNumber = new int[]{1,2,3,4};
+        SearchResult searchResult = BinarySearch.search(6, arrayOfNumber);
+        assertEquals(false, searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
     }
 
 }
