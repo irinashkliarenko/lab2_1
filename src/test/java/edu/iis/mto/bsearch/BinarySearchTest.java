fe1f2cb852a0cf3490683205c7e4c98467ce483d
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-20 13:41:08 +0100

    testy_v3 add test elementIsInMiddleOfSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.bsearch;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 
@@ -67,16 +68,17 @@ public class BinarySearchTest {
         assertThat(searchResult.getPosition(), is((seq.length-1)/2) );
     }
 
-
     @Test(expected = IllegalArgumentException.class)
     public void emptySequence() throws Exception {
         BinarySearch.search(element, new int[0]);
     }
 
+    @Test
+    public void elementIsInMiddleOfSequence() throws Exception {
 
-
-
-
-
-
+        seq = new int[] {element - 235, element - 53, element, element + 56, element + 666};
+        searchResult = BinarySearch.search(element, seq);
+        assertThat(searchResult.isFound(), is(true));
+        assertThat(searchResult.getPosition(), equalTo(2));
+    }
 }
