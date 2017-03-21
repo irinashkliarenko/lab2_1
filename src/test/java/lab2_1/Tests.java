8c2208f97e4a1e2079f2444864a95be1f92770b0
195024 <195024@edu.p.lodz.pl>
2017-03-21 12:26:23 +0100

    testy_v3 testy dla środek+1 i środek-1 & poprawa formatowania

--- src/test/java/lab2_1/Tests.java
+++ src/test/java/lab2_1/Tests.java
@@ -1,7 +1,5 @@
 package lab2_1;
 
-import static org.junit.Assert.*;
-
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,7 +12,7 @@ public class Tests {
 	public void sequenceLengthIsOneAndElementIsInTheSequence() {
 		int[] seq = new int[1];
 		seq[0] = 1;
-		
+
 		Assert.assertThat(BinarySearch.search(1, seq).isFound(), Matchers.equalTo(true));
 	}
 
@@ -22,7 +20,7 @@ public class Tests {
 	public void sequenceLengthIsOneAndElementIsNotInTheSequence() {
 		int[] seq = new int[1];
 		seq[0] = 52;
-		
+
 		Assert.assertThat(BinarySearch.search(1, seq).isFound(), Matchers.equalTo(false));
 	}
 
@@ -31,63 +29,89 @@ public class Tests {
 		int[] seq = new int[2];
 		seq[0] = 1;
 		seq[1] = 2;
-		
+
 		Assert.assertThat(BinarySearch.search(1, seq).getPosition(), Matchers.equalTo(0));
 	}
 
 	@Test
 	public void sequenceLengthIsLongerThanOneAndElementIsLast() {
 		int[] seq = new int[3];
-		
-		for(int i : seq) {
+
+		for (int i : seq) {
 			seq[i] = 0;
 		}
-		
+
 		seq[2] = 122;
-		
+
 		Assert.assertThat(BinarySearch.search(122, seq).getPosition(), Matchers.equalTo(seq.length - 1));
 	}
 
 	@Test
 	public void sequenceLengthIsLongerThanOneAndElementIsTheMiddleOne() {
 		int[] seq = new int[5];
-		
-		for(int i : seq) {
+
+		for (int i : seq) {
 			seq[i] = 0;
 		}
-		
+
 		seq[2] = 2;
 
 		Assert.assertThat(BinarySearch.search(2, seq).getPosition(), Matchers.equalTo(seq.length / 2));
 	}
-	
+
 	@Test
 	public void sequenceLengthIsLongerThanOneAndElementIsNotFound() {
 		int[] seq = new int[6];
-		
-		for(int i : seq) {
+
+		for (int i : seq) {
 			seq[i] = 0;
 		}
 
 		seq[1] = 67;
-		
+
 		Assert.assertThat(BinarySearch.search(67, seq).isFound(), Matchers.equalTo(false));
 	}
-	
-	@Test (expected = IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void sequenceLengthIsZero() {
 		int[] seq = new int[0];
 		BinarySearch.search(2, seq);
 	}
-	
+
 	@Test
 	public void getPositionForNotFoundElementReturnsNegativeOne() {
 		int[] seq = new int[10];
-		
-		for(int i : seq) {
+
+		for (int i : seq) {
 			seq[i] = 0;
 		}
-		
+
 		Assert.assertThat(BinarySearch.search(12, seq).getPosition(), Matchers.equalTo(-1));
+	}
+
+	@Test
+	public void positionOfCenterSubOneElementIsCorrect() {
+		final int size = 37;
+		int[] seq = new int[size];
+		final int centerSubOne = (size / 2) - 1;
+
+		for (int i = 0; i < size; i++) {
+			seq[i] = i;
+		}
+
+		Assert.assertThat(BinarySearch.search(centerSubOne, seq).getPosition(), Matchers.equalTo(centerSubOne));
+	}
+
+	@Test
+	public void positionOfCenterPlusOneElementIsCorrect() {
+		final int size = 49;
+		int[] seq = new int[size];
+		final int centerPlusOne = (size / 2) + 1;
+
+		for (int i = 0; i < size; i++) {
+			seq[i] = i;
+		}
+
+		Assert.assertThat(BinarySearch.search(centerPlusOne, seq).getPosition(), Matchers.equalTo(centerPlusOne));
 	}
 }
