6800283f317283577a55b1102ca3c873ae978fa2
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-21 10:50:56 +0100

    testy_v1 Cases: Key is last in sequence & seq.lenght()>1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -25,10 +25,19 @@ public class BinarySearchTest {
     }
 
     @Test public void testIfKeyIsFirstInSeq (){
-        int key = 5;
+        int key = 2;
         int[] seq = {2,5,7};
         SearchResult result = BinarySearch.search(key,seq);
         Assert.assertTrue(result.isFound());
         Assert.assertTrue(result.getPosition()==1);
     }
+
+    @Test public void testIfKeyIsLastInSeq() {
+        int key = 5;
+        int[] seq = {1,3,5};
+        SearchResult result =   BinarySearch.search(key,seq);
+        Assert.assertTrue(result.isFound());
+        Assert.assertTrue(result.getPosition()==seq.length);
+    }
+
 }
